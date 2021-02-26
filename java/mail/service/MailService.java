e5435fd887f44eb59a560f830fcd1c9b1a499258
ali8210225786 <ali8210225786@gmail.com>
2021-02-26 14:39:15 +0800

    驗證信

---
+++ java/mail/service/MailService.java
@@ -0,0 +1,32 @@
+package mail.service;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mail.dao.MailDao;
+
+@Service
+public class MailService {
+
+	@Autowired
+	SessionFactory factory;
+	
+	@Autowired
+	MailDao dao;
+	
+	public MailService() {
+		
+	}
+	
+	@Transactional
+	public void changeVerification(String type , String email , String hash) {
+			dao.changeVerification(type, email, hash);
+	}
+	
+	
+	
+	
+}
