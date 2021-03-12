ec7478ce5779e3cf9c2868448a7a9b97931a0721
ali8210225786 <ali8210225786@gmail.com>
2021-03-12 17:55:50 +0800

    .

--- java/_00_init/config/RootAppConfig.java
+++ java/_00_init/config/RootAppConfig.java
@@ -25,7 +25,7 @@ public class RootAppConfig {
     public DataSource dataSource() {
         ComboPooledDataSource ds = new ComboPooledDataSource();
         ds.setUser("root");
-        ds.setPassword("123456");
+        ds.setPassword("root");
         try {
             ds.setDriverClass("com.mysql.cj.jdbc.Driver");
         } catch (PropertyVetoException e) {
