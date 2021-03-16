470debf2feff40e27fd2d1ddafdb2e3f7b1064d6
tsou8599 <tsou8599@gmail.com>
2021-03-16 18:07:43 +0800

    修改首頁人氣教練(可以透過評價篩選人氣教練)

--- java/_03_memberData/service/MemberDataService.java
+++ java/_03_memberData/service/MemberDataService.java
@@ -1,7 +1,10 @@
 package _03_memberData.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import javax.transaction.Transactional;
@@ -16,6 +19,8 @@ import _03_memberData.dao.MemberDataDao;
 import _03_memberData.model.Area_H;
 import _03_memberData.model.City_H;
 import _03_memberData.model.TrainerLicenseBean_H;
+import _09_trainerCourse.model.RatingsAvgBean_H;
+import _09_trainerCourse.model.RatingsBean_H;
 import _09_trainerCourse.model.TrainerCourseBean_H;
 
 @Transactional
@@ -71,29 +76,42 @@ public class MemberDataService {
 		return memberDataDao.getTrainerLicenseById(id);
 	}
 
+	public List<RatingsBean_H> getTrainerRatings(int id) {
+
+		return memberDataDao.getTrainerRatings(id);
+	}
+
 	public List<TrainerCourseBean_H> getTrainerAndCourse() {
 
 		// 存放TrainerCourseBean_H內容的集合
 		List<TrainerCourseBean_H> list = memberDataDao.getTrainerAndCourse();
-		// 存放亂序結果的集合
+
+		// 存放篩選評價後結果的集合
+		ArrayList<TrainerCourseBean_H> filterRatings = new ArrayList<>();
+		// 存放亂序篩選評價結果的集合
 		ArrayList<TrainerCourseBean_H> result = new ArrayList<>();
 		Random random = new Random();
-		// 首頁要隨機產生六個推薦教練，若是資料庫沒有六筆以上教練資料，就產生當前資料庫的教練資料筆數
-		if (list.size() < 6) {
-			for (int i = 0; i < list.size(); i++) {
-				// 將取出的這個元素放到存放亂序結果的集合中
-				result.add(list.get(i));
-			}
-		} else {
-			for (int i = 0; i < 6; i++) {
-				int myRand = random.nextInt(list.size());
-				// 將取出的這個元素放到存放亂序結果的集合中
-				result.add(list.get(myRand));
-				// 從原始集合中刪除該元素防止重複
-				list.remove(myRand);
+		//篩選教練評價
+		for (int j = 0; j < list.size(); j++) {
+			if (list.get(j).getTrainerBean_H().getRatings() > 4 ) {
+				filterRatings.add(list.get(j));
 			}
 		}
+		// 首頁要隨機產生六個推薦教練，若是資料庫沒有六筆以上教練資料，就產生當前資料庫的教練資料筆數
+				if (filterRatings.size() < 6) {
+					for (int i = 0; i < filterRatings.size(); i++) {
+						// 將取出的這個元素放到存放亂序結果的集合中
+						result.add(filterRatings.get(i));
+					}
+				} else {
+					for (int i = 0; i < 6; i++) {
+						int myRand = random.nextInt(filterRatings.size());
+						// 將取出的這個元素放到存放亂序結果的集合中
+						result.add(filterRatings.get(myRand));
+						// 從原始集合中刪除該元素防止重複
+						filterRatings.remove(myRand);
+					}
+				}
 		return result;
 	}
 }
-
